e31f6a457aae6274d0eb0616750552bb2d31f6e2
SIRAMRULWA <sihlemrulwa@gmail.com>
2023-04-13 07:59:11 +0200

    Responsive Web

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function myFunction(){
+    let name= document.getElementById("fname").value;
+    let surname= document.getElementById("lname").value;
+    let email= document.getElementById("email").value;
+    let gender= document.getElementById("gender").value;
+    let course = document.getElementById("course").value;
+    console.log(name);
+    console.log(surname);
+    console.log(email);
+    console.log(gender);
+    console.log(course);
+
+ }
